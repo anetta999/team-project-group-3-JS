b28b93ec53731b82d595a2c35a898bf9c32f2f29
Fetivol <Fetisowova@gmail.com>
2023-08-05 03:41:31 +0200

    updated categories.list

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,8 +1,4 @@
-import {
-  fetchCategoryList,
-  fetchTopBooks,
-  fetchSelectCategory,
-} from './api.js';
+import { fetchSelectCategory } from './api.js';
 
 const listEl = document.querySelector('.categories_list');
 const booksContainer = document.querySelector('.books-container');
@@ -46,16 +42,6 @@ async function displayCategory(chosenCategory) {
 function stringFirstSpace(string) {
   return string.indexOf(' ');
 }
-// function createCategoryTitleMarkap(title, element) {
-//   console.log(stringFirstSpace(element));
-//   title = `<h2 class="category-name">${element.substring(
-//     0,
-//     stringFirstSpace(element)
-//   )}<span class = "title-color">${element.substring(
-//     stringFirstSpace(element)
-//   )}</span></h2>`;
-//   return title;
-// }
 
 //Creating Markap for each book from category
 function createCategoryBooksMarkap(arr) {
@@ -71,44 +57,3 @@ function createCategoryBooksMarkap(arr) {
     })
     .join('');
 }
-
-//Stas part
-// showCategory('Picture Books');
-
-// async function displayTopBooks() {
-//   try {
-//     const response = await fetchTopBooks();
-//     // console.log(response);
-//     booksContainer.insertAdjacentHTML('beforeend', createTopBookMarkap(response));
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// function createTopBookMarkap(arr) {
-//   return arr
-//     .map(
-//       ({
-//         list_name,
-//         books: [{ _id }],
-//         books: [{ amazon_product_url }],
-//         books: [{ book_image }],
-//         books: [{ author }],
-//         books: [{ title }],
-//         books: [{ book_image_width }],
-//         books: [{ book_image_height }],
-//       }) => {
-//         return `
-//       <h2>${list_name}</h2>
-//       <div data-id = '${_id}'>
-//       <a href='${amazon_product_url}'>
-//       <img src='${book_image}' alt='${author},${title}' width = '${book_image_width}' hight = '${book_image_height}' loading='lazy' />
-//       <div><h3>${title}</h3>
-//       <p>${author}</p>
-//       </div></a></div>
-//   `;
-//       }
-//     )
-//     .join('');
-// }
-// displayTopBooks();
