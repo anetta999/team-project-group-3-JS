d15dd65ec6103c768fcd534406ed6256c88f7eeb
Fetivol <Fetisowova@gmail.com>
2023-08-08 11:18:21 +0200

    add open loader to modal window

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,6 +6,7 @@ import aplle_2x from '../img/image2@2x.png';
 import bookshop from '../img/image3@1x.png';
 import bookshop_2x from '../img/image3@2x.png';
 import svg from '../img/sprite.svg';
+import { showLoader, hideLoader } from './loader.js';
 
 const bodyScrollLock = require('body-scroll-lock');
 
@@ -14,99 +15,95 @@ const list = document.querySelector('.books-container-list');
 const backdrop = document.querySelector('.backdrop');
 const targetElement = document.querySelector('#no-scroll');
 
-const disableBodyScroll = bodyScrollLock.disableBodyScroll; 
+const disableBodyScroll = bodyScrollLock.disableBodyScroll;
 const enableBodyScroll = bodyScrollLock.enableBodyScroll;
-const BOOK_LS_KEY = 'shopplist'
+const BOOK_LS_KEY = 'shopplist';
 
 list.addEventListener('click', showBook);
 backdrop.addEventListener('click', onClickBackdrop);
 
 async function showBook(evt) {
-    evt.preventDefault()
-if(evt.target.nodeName !== "IMG"){
-    return
-}
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  showLoader();
+  const bookId = evt.target.id;
 
-const bookId = evt.target.id
-
-    let addList = JSON.parse(localStorage.getItem(BOOK_LS_KEY)) || []
+  let addList = JSON.parse(localStorage.getItem(BOOK_LS_KEY)) || [];
 
   try {
     const data = await fetchBookId(bookId);
     modal.innerHTML = createMarkupCardModal(data);
-
-    const closeBtn = document.querySelector('.modal-close-btn'); 
+    hideLoader();
+    const closeBtn = document.querySelector('.modal-close-btn');
     closeBtn.addEventListener('click', closeModal);
-   
+
     backdrop.classList.remove('is-hidden');
     window.addEventListener('keydown', onPressESC);
     disableBodyScroll(targetElement);
-   
 
     const addBtn = document.querySelector('.modal-add-btn');
-    const modalEl = document.querySelector('.modal-remove-text')
+    const modalEl = document.querySelector('.modal-remove-text');
 
-    if(addList.some((item) => item._id === bookId)) {
-        addBtn.textContent = 'remove from the shopping list';
-        addBtn.classList.replace('modal-add-btn','modal-remove-btn');
-        modalEl.classList.toggle('is-hidden');
+    if (addList.some(item => item._id === bookId)) {
+      addBtn.textContent = 'remove from the shopping list';
+      addBtn.classList.replace('modal-add-btn', 'modal-remove-btn');
+      modalEl.classList.toggle('is-hidden');
 
-        const removeBtn = document.querySelector('.modal-remove-btn');
-    removeBtn.addEventListener('click', evt => {
-        removeFromShoppList(evt, removeBtn, modalEl, data)
-             })
+      const removeBtn = document.querySelector('.modal-remove-btn');
+      removeBtn.addEventListener('click', evt => {
+        removeFromShoppList(evt, removeBtn, modalEl, data);
+      });
     } else {
-        addBtn.addEventListener('click', evt => {
-       
-            addToShoppList(evt, addBtn, modalEl, data)
-        })
+      addBtn.addEventListener('click', evt => {
+        addToShoppList(evt, addBtn, modalEl, data);
+      });
     }
   } catch (error) {
     console.error(error);
   }
 }
 
-function addToShoppList(evt, btn, modEl, data){
-    if(!evt.target.classList.contains('modal-add-btn')){
-        return
-    } 
-    let addList = JSON.parse(localStorage.getItem(BOOK_LS_KEY)) || []
-   addList.push(data)
-   localStorage.setItem(BOOK_LS_KEY, JSON.stringify(addList));
-    
-    btn.textContent = 'remove from the shopping list';
-    btn.classList.replace('modal-add-btn','modal-remove-btn');
-    modEl.classList.toggle('is-hidden');
+function addToShoppList(evt, btn, modEl, data) {
+  if (!evt.target.classList.contains('modal-add-btn')) {
+    return;
+  }
+  let addList = JSON.parse(localStorage.getItem(BOOK_LS_KEY)) || [];
+  addList.push(data);
+  localStorage.setItem(BOOK_LS_KEY, JSON.stringify(addList));
 
-    const removeBtn = document.querySelector('.modal-remove-btn');
-    removeBtn.addEventListener('click', evt => {
-        removeFromShoppList(evt, removeBtn, modEl, data)
-             })
+  btn.textContent = 'remove from the shopping list';
+  btn.classList.replace('modal-add-btn', 'modal-remove-btn');
+  modEl.classList.toggle('is-hidden');
 
+  const removeBtn = document.querySelector('.modal-remove-btn');
+  removeBtn.addEventListener('click', evt => {
+    removeFromShoppList(evt, removeBtn, modEl, data);
+  });
 }
 
 function removeFromShoppList(evt, remBtn, modalEl, data) {
-    if(!evt.target.classList.contains('modal-remove-btn')){
-        return
-    }
-    const curID = evt.target.closest('.modal-id')
-    const book = curID.dataset.id;
-    let addList = JSON.parse(localStorage.getItem(BOOK_LS_KEY)) || [];
-    const deleteData = addList.filter((item) => item._id !== book)
-    localStorage.setItem(BOOK_LS_KEY, JSON.stringify(deleteData));
+  if (!evt.target.classList.contains('modal-remove-btn')) {
+    return;
+  }
+  const curID = evt.target.closest('.modal-id');
+  const book = curID.dataset.id;
+  let addList = JSON.parse(localStorage.getItem(BOOK_LS_KEY)) || [];
+  const deleteData = addList.filter(item => item._id !== book);
+  localStorage.setItem(BOOK_LS_KEY, JSON.stringify(deleteData));
 
-    remBtn.textContent = 'add to shopping list';
-    remBtn.classList.replace('modal-remove-btn', 'modal-add-btn');
-    modalEl.classList.toggle('is-hidden');
-    
-    remBtn.addEventListener('click', evt => {
-       
-        addToShoppList(evt, remBtn, modalEl, data)
-    })
+  remBtn.textContent = 'add to shopping list';
+  remBtn.classList.replace('modal-remove-btn', 'modal-add-btn');
+  modalEl.classList.toggle('is-hidden');
+
+  remBtn.addEventListener('click', evt => {
+    addToShoppList(evt, remBtn, modalEl, data);
+  });
 }
 
 function createMarkupCardModal(arr) {
-  let {_id, author, title, book_image, description, buy_links } = arr;
+  let { _id, author, title, book_image, description, buy_links } = arr;
   return `
   <button type="button" class="modal-close-btn dark-close-btn" data-modal-close>
         <svg class="modal-close-png dark-close-png">
@@ -161,25 +158,24 @@ function createMarkupCardModal(arr) {
     <p class="modal-remove-text dark-modal-remove-text is-hidden">Сongratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.</p>
     </div>
        `;
-};
+}
 
 function closeModal() {
   backdrop.classList.add('is-hidden');
   window.removeEventListener('keydown', onPressESC);
-  enableBodyScroll(targetElement)
- 
-};
+  enableBodyScroll(targetElement);
+}
 
 function onPressESC(evt) {
   if (evt.code === 'Escape') {
     closeModal();
     window.removeEventListener('keydown', onPressESC);
-    enableBodyScroll(targetElement)
+    enableBodyScroll(targetElement);
   }
-};
+}
 
 function onClickBackdrop(evt) {
-      if (evt.target.classList.contains('backdrop')) {
-        closeModal();
-      }
-};
+  if (evt.target.classList.contains('backdrop')) {
+    closeModal();
+  }
+}
