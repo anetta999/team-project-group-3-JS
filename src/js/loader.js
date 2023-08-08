0867249399a21fa1a7c8a2bf13d4b4fb62327d55
Fetivol <Fetisowova@gmail.com>
2023-08-08 10:59:38 +0200

    update for scroll-up button

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,13 +1,12 @@
+const loaderElement = document.querySelector('.loader-backdrop');
+const bodyElement = document.body;
+
 function showLoader() {
-  const loaderElement = document.querySelector('.loader-backdrop');
-  const bodyElement = document.body;
   loaderElement.classList.remove('is-hidden');
   bodyElement.classList.add('loader-open');
 }
 
 function hideLoader() {
-  const loaderElement = document.querySelector('.loader-backdrop');
-  const bodyElement = document.body;
   loaderElement.classList.add('is-hidden');
   bodyElement.classList.remove('loader-open');
 }
