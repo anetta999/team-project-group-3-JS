d0f6e0cf4fdbeefab8c52170c525d0ca88d38787
Fetivol <Fetisowova@gmail.com>
2023-08-10 15:04:25 +0200

    update for Loader + string length for title and author

--- src/js/loader.js
+++ src/js/loader.js
@@ -2,12 +2,14 @@ const loaderElement = document.querySelector('.loader-backdrop');
 const bodyElement = document.body;
 
 function showLoader() {
-  loaderElement.classList.remove('is-hidden');
+  loaderElement.classList.remove('loader-is-hidden');
+  loaderElement.classList.add('loader-backdrop');
   bodyElement.classList.add('loader-open');
 }
 
 function hideLoader() {
-  loaderElement.classList.add('is-hidden');
+  loaderElement.classList.add('loader-is-hidden');
+  loaderElement.classList.remove('loader-backdrop');
   bodyElement.classList.remove('loader-open');
 }
 
